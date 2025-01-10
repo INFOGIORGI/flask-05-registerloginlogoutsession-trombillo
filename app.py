782abd969bde0964717e8189f4e5eab5789c185e
trombillo <marcoramp12@gmail.com>
2025-01-10 12:39:04 +0000

    esercizio iniziato in classe

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask , render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def register():
+    return render_template("register.html")
 
-app.run()
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    return render_template("logout.html")
+
+app.run(debug=True)
