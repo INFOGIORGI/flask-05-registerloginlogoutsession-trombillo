4f83f868cb3a21402a598f053a96f7d742728787
trombillo <marcoramp12@gmail.com>
2025-01-14 11:48:51 +0000

    k

--- app.py
+++ app.py
@@ -1,17 +1,40 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST']=
+app.config['MYSQL_HOST']=
+app.config['MYSQL_HOST']=
+app.config['MYSQL_HOST']=
+app.config['MYSQL_HOST']=
+
 @app.route("/")
-def register():
-    return render_template("register.html")
+def home():
+    return render_template("index.html", titolo="Home")
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    return render_template("login.html", titolo="Login")
 
-@app.route("/logout")
-def logout():
-    return render_template("logout.html")
+@app.route("/register", methods=['POST','GET'])
+def register():
+    if request.method=='GET':
+        return render_template("register.html", titolo="register")
+    fname = request.form ['fname']
+    lname = request.form['lname']
+    username = request.form['username']
+    password = request.form['password']
+    confirm_password = request.form['confirm_password']
+
+    if password == confirm_password:
+        cursor=mysql.connection.cursor()
+        query="INSERT INTO users VALUES (%s, %s, %s, %s)"
+        cursor.execute(query, (username, password, fname, lname))
+        mysql.connection.commit()
+        cursor.execute("SELECT * FROM users")
+        print(cursor.fetchall())
+        return redirect(url_for('home'))
+    return "passord diverse"
 
 app.run(debug=True)
